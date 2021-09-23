8a63c157c4b8d3ecb3ac1d47f4ea68f9271015de
Alex Tkachenko <skyalexx@gmail.com>
2021-09-23 22:45:35 +0300

    User entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -15,29 +15,24 @@ import {
   Unpaused,
   WonNftClaimed
 } from "../generated/Auction/Auction"
-import { ActiveListingID, Listing, NFT } from "../generated/schema"
+import { ActiveListingID, Listing, NFT, User } from "../generated/schema"
 
 
 export function handleAuctionCreated(event: AuctionCreated): void {
-  const _nftId = nftId(event.params.nft, event.params.nftId)
+  const _nftId = getNftId(event.params.nft, event.params.nftId)
   const listingId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
 
   const _id = new ActiveListingID(_nftId)
   _id.listingId = listingId
   _id.save()
 
-  let nft = NFT.load(_nftId)
-  if (!nft) {
-    nft = new NFT(_nftId)
-    nft.address = event.params.nft
-    nft.tokenId = event.params.nftId
-    nft.save()
-  }
+  const from = createUser(event.transaction.from)
+  const nft = createNft(event.params.nft, event.params.nftId)
 
   const listing = new Listing(listingId)
   listing.status = 'Created'
   listing.nft = nft.id
-  listing.author = event.transaction.from
+  listing.author = from.id
   listing.endTimestamp = new BigInt(0)
   listing.save()
 }
@@ -46,7 +41,10 @@ export function handleAuctionCanceled(event: AuctionCanceled): void {
   const id = findActiveNftListingId(event.params.nft, event.params.nftId)
   if (id === null) return
 
-  store.remove('ActiveListingID', nftId(event.params.nft, event.params.nftId))
+  // can be called by admin who hasn't been created before
+  createUser(event.transaction.from)
+
+  store.remove('ActiveListingID', getNftId(event.params.nft, event.params.nftId))
 
   const listing = new Listing(id)
   listing.status = 'Cancelled'
@@ -56,10 +54,12 @@ export function handleAuctionCanceled(event: AuctionCanceled): void {
 export function handleBidSubmitted(event: BidSubmitted): void {
   const id = findActiveNftListingId(event.params.nft, event.params.nftId)
   if (id === null) return
+
+  const bidder = createUser(event.transaction.from)
   
   const listing = new Listing(id)
   listing.status = 'Active'
-  listing.bidder = event.params.bidder
+  listing.bidder = bidder.id
   listing.bid = event.params.amount
   listing.endTimestamp = event.params.endTimestamp
   listing.save()
@@ -68,6 +68,9 @@ export function handleBidSubmitted(event: BidSubmitted): void {
 export function handleReservePriceChanged(event: ReservePriceChanged): void {
   const id = findActiveNftListingId(event.params.nft, event.params.nftId)
   if (id === null) return
+
+  // can be called by admin who hasn't been created before
+  createUser(event.transaction.from)
 
   let listing = new Listing(id)
   listing.bid = event.params.startPrice
@@ -78,7 +81,7 @@ export function handleWonNftClaimed(event: WonNftClaimed): void {
   const id = findActiveNftListingId(event.params.nft, event.params.nftId)
   if (id === null) return
   
-  store.remove('ActiveListingID', nftId(event.params.nft, event.params.nftId))
+  store.remove('ActiveListingID', getNftId(event.params.nft, event.params.nftId))
   
   const listing = new Listing(id)
   listing.status = 'Finished'
@@ -87,11 +90,34 @@ export function handleWonNftClaimed(event: WonNftClaimed): void {
 
 // returns id of active Listing for specific NFT token
 function findActiveNftListingId(nft: Address, tokenId: BigInt): string | null  {
-  const id = ActiveListingID.load(nftId(nft, tokenId))
+  const id = ActiveListingID.load(getNftId(nft, tokenId))
   return id ? id.listingId : null
 }
 
 // returns string id of NFT token
-function nftId(nft: Address, tokenId: BigInt): string {
+function getNftId(nft: Address, tokenId: BigInt): string {
   return nft.toHexString() + '-' + tokenId.toHexString()
+}
+
+// creates user entity if not created yet and returns it
+function createUser(address: Address): User {
+  let user = User.load(address.toHexString())
+  if (user === null) {
+    user = new User(address.toHexString())
+    user.save()
+  }
+  return user
+}
+
+// creates nft entity if not created yet and returns it
+function createNft(nftAddress: Address, tokenId: BigInt): NFT {
+  const nftId = getNftId(nftAddress, tokenId)
+  let nft = NFT.load(nftId)
+  if (nft === null) {
+    nft = new NFT(nftId)
+    nft.address = nftAddress
+    nft.tokenId = tokenId
+    nft.save()
+  }
+  return nft
 }
